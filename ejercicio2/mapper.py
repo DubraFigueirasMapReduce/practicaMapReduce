e96504509523ee21dbe1eed0d6f9c25a3bb86da9
dubrajennifer <dubrajennifer@gmail.com>
2021-10-26 20:36:02 +0200

    mapper ej2

--- ejercicio2/mapper.py
+++ ejercicio2/mapper.py
@@ -8,3 +8,9 @@ for line in sys.stdin:
   line = line.strip()
   # split the line into values
   values = line.split()
+  file_name=os.environ['mapreduce_map_input_file']
+  s1=file_name.split('/')
+  file_name=s1[-1]
+  file_name=file_name.split('.')
+  user = file_name[0] #conXX
+  print '%s\t%s' % (user, 1) 
