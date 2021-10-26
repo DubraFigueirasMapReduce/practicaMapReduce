736c684d7762a0bee3f006ef8033dae210a9cf45
Jose Figueiras <50220451+josefigueirasm@users.noreply.github.com>
2021-10-26 20:21:28 +0200

    Reductor funcionando

--- ejercicio1/reducer.py
+++ ejercicio1/reducer.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python
 import sys
+
+NULL = "-9999.0"
+minCheck = 0
+
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    clean_line = line.strip()
+    # parse the input we got from mapper.py
+    temp, city = clean_line.split()
+    if temp != NULL and minCheck == 0:
+        print clean_line
+        minCheck = 1
+    max_line = clean_line
+print max_line
