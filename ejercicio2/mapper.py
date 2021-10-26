abb8f2833a3595e584fa115977e00630952cc463
Jose Figueiras <50220451+josefigueirasm@users.noreply.github.com>
2021-10-26 21:28:10 +0200

    Mapper ejercicio 2

--- ejercicio2/mapper.py
+++ ejercicio2/mapper.py
@@ -9,15 +9,14 @@ s1=file_name.split('/')
 file_name=s1[-1]
 file_name=file_name.split('.')
 user = file_name[0] #conXX
-  
+
 for line in sys.stdin:
   # remove leading and trailing whitespace
   line = line.strip()
   # split the line into values
   entries = line.split()
   url = entries[3]
-  urlfiletype = split(".")
+  urlfiletype = url.split(".")
   urlfiletype = urlfiletype[-1]
-  if urlfiletype == "ps":	
+  if urlfiletype[:-1] == "ps":	
     print '%s\t%s' % (user, 1) 
-  
