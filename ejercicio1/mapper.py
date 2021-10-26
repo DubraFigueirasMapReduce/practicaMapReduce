08060ddf5d18fc9b71568143f6508ad40921a0a2
Jose Figueiras <50220451+josefigueirasm@users.noreply.github.com>
2021-10-26 19:38:05 +0200

    mapper que ordena por valor de temperatura

--- ejercicio1/mapper.py
+++ ejercicio1/mapper.py
@@ -6,11 +6,11 @@ import sys
 for line in sys.stdin:
   # remove leading and trailing whitespace
   line = line.strip()
-  ## split the line into values
+  # split the line into values
   values = line.split()
   file_name=os.environ['mapreduce_map_input_file']
   s1=file_name.split('/')
   file_name=s1[-1]
-  file_name=filename[16:len(file_name)-11) #get city name
-  print '%s\t%s' % (value[12], file_name) #min temp
-  print '%s\t%s' % (value[13], file_name) #max temp
+  file_name=file_name[17:len(file_name)-11] #get city name
+  print '%s\t%s' % (values[12], file_name) #min temp
+  print '%s\t%s' % ( values[13], file_name) #max temp
