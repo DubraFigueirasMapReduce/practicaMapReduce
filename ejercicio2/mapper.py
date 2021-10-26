beb953344c9fbc0a20d781be412cdb9cb3a905b3
dubrajennifer <dubrajennifer@gmail.com>
2021-10-26 20:51:58 +0200

    mapper ej2

--- ejercicio2/mapper.py
+++ ejercicio2/mapper.py
@@ -3,14 +3,21 @@
 import os
 import sys
 
+
+file_name=os.environ['mapreduce_map_input_file']
+s1=file_name.split('/')
+file_name=s1[-1]
+file_name=file_name.split('.')
+user = file_name[0] #conXX
+  
 for line in sys.stdin:
   # remove leading and trailing whitespace
   line = line.strip()
   # split the line into values
-  values = line.split()
-  file_name=os.environ['mapreduce_map_input_file']
-  s1=file_name.split('/')
-  file_name=s1[-1]
-  file_name=file_name.split('.')
-  user = file_name[0] #conXX
-  print '%s\t%s' % (user, 1) 
+  entries = line.split()
+  url = entries[3]
+  urlfiletype = split(".")
+  urlfiletype = urlfiletype[-1]
+  if urlfiletype == "ps":	
+    print '%s\t%s' % (user, 1) 
+  
