69f69494281fe0c263594f4d1040455ecbed0eb5
Jose Figueiras <50220451+josefigueirasm@users.noreply.github.com>
2021-10-28 16:21:24 +0200

    Optimizado ejercicio 1

--- ejercicio1/mapper.py
+++ ejercicio1/mapper.py
@@ -3,14 +3,18 @@
 import os
 import sys
 
+# Get city name
+file_name=os.environ['mapreduce_map_input_file']
+s1=file_name.split('/')
+file_name=s1[-1]
+file_name=file_name[17:len(file_name)-11] 
+
 for line in sys.stdin:
   # remove leading and trailing whitespace
   line = line.strip()
   # split the line into values
   values = line.split()
-  file_name=os.environ['mapreduce_map_input_file']
-  s1=file_name.split('/')
-  file_name=s1[-1]
-  file_name=file_name[17:len(file_name)-11] #get city name
-  print '%s\t%s' % (values[12], file_name) #min temp
-  print '%s\t%s' % ( values[13], file_name) #max temp
+  #min temp
+  print '%s\t%s' % (values[12], file_name)
+  #max temp
+  print '%s\t%s' % ( values[13], file_name)
--- ejercicio1/reducer.py
+++ ejercicio1/reducer.py
@@ -9,8 +9,11 @@ for line in sys.stdin:
     clean_line = line.strip()
     # parse the input we got from mapper.py
     temp, city = clean_line.split()
+    # Check if the value is valid and it's the first time it enters
     if temp != NULL and minCheck == 0:
         print clean_line
         minCheck = 1
+    # The last time the loop asigns this variable is the last line, so it's the
+    # max temperature
     max_line = clean_line
 print max_line
