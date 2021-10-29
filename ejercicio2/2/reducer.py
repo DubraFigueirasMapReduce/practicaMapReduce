c2183f12f51b3e837b7bbfecfd563793940d0ba2
Jose Figueiras <50220451+josefigueirasm@users.noreply.github.com>
2021-10-29 02:40:57 +0200

    Subida ejercicio 2

--- ejercicio2/2/reducer.py
+++ ejercicio2/2/reducer.py
@@ -7,10 +7,10 @@
 from operator import itemgetter #No lo esta usando
 import sys
 
-current_user = None
+current_url = None
 current_count = 0
-user = None
-max_user = None
+url = None
+max_url = None
 max_count = 0
 first_execution = True
 
@@ -20,9 +20,8 @@ for line in sys.stdin:
     line = line.strip()
 
     # parse the input we got from mapper.py
-    user, count = line.split('\t', 1)
+    url, count = line.split('\t', 1)
 
-    
     # convert count (currently a string) to int
     try:
         count = int(count)
@@ -30,21 +29,21 @@ for line in sys.stdin:
         # count was not a number, so silently
         # ignore/discard this line
         continue
-    
+
     if first_execution:
-        max_user = user
+        max_url = url
         max_count = count
         first_execution = False
 
     # this IF-switch only works because Hadoop sorts map output
-    # by key (here: user) before it is passed to the reducer
-    if current_user == user:
+    # by key (here: url) before it is passed to the reducer
+    if current_url == url:
         current_count += count
         # It is checked if after the increment the user has the most accesess
         if current_count >= max_count:
-            max_user = user
+            max_url = url
             max_count = current_count
     else:
         current_count = count
-        current_user = user
-print '%s\t%s' % (max_user, max_count)
+        current_url = url
+print '%s\t%s' % (max_url, max_count)
