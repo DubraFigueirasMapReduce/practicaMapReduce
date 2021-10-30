cf3ee9f5e1d69c82dd62662da8b4be3640a83fcd
Jose Figueiras <50220451+josefigueirasm@users.noreply.github.com>
2021-10-30 12:45:47 +0200

    Mapper ejercicio 3

--- ejercicio3/mapper.py
+++ ejercicio3/mapper.py
@@ -3,26 +3,25 @@
 import os
 import sys
 
-first_execution = True
-
 # Get winetype
 file_name=os.environ['mapreduce_map_input_file']
 s1=file_name.split('/')
 file_name=s1[-1]
-file_name=file_name[12:len(file_name)-4] 
+file_name=file_name[12:len(file_name)-4]
+
+NOT_VALID_LINE = '"fixed acidity";"volatile acidity";"citric acid";"residual sugar";"chlorides";"free sulfur dioxide";"total sulfur dioxide";"density";"pH";"sulphates";"alcohol";"quality"'
+attribute_names = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
 
 for line in sys.stdin:
   # remove leading and trailing whitespace
   line = line.strip()
+  # It cannot count the first line of the document. It is not valid.
+  if line == NOT_VALID_LINE:
+    continue
   # split the line into values
-  winesubtype = line.split(";")
-  
-  if first_execution:
-        attributes = winesubtype
-        first_execution = False
-        
-  if (!first_execution)
-    nAttr = 0;
-    for attr in winesubtype:
-      print '%s\t%s' % (file_name + "-" + attributes[nAttr], attr) 
-      nAttr += 1
+  winesubtype = line.split(';')
+
+  nAttr = 0
+  for attr in winesubtype:
+    print '%s\t%s' % (file_name + '-' + attribute_names[nAttr], attr) 
+    nAttr += 1
