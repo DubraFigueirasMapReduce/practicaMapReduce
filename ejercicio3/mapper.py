cbcdbc1855b6d3a6e30fcbd7eba0f3a935e9f70e
dubrajennifer <dubrajennifer@gmail.com>
2021-10-30 12:19:06 +0200

    mapper ej3

--- ejercicio3/mapper.py
+++ ejercicio3/mapper.py
@@ -3,8 +3,26 @@
 import os
 import sys
 
+first_execution = True
+
+# Get winetype
+file_name=os.environ['mapreduce_map_input_file']
+s1=file_name.split('/')
+file_name=s1[-1]
+file_name=file_name[12:len(file_name)-4] 
+
 for line in sys.stdin:
   # remove leading and trailing whitespace
   line = line.strip()
   # split the line into values
-  entries = line.split()
+  winesubtype = line.split(";")
+  
+  if first_execution:
+        attributes = winesubtype
+        first_execution = False
+        
+  if (!first_execution)
+    nAttr = 0;
+    for attr in winetype:
+      print '%s\t%s' % (file_name + "-" + attributes[nAttr], attr) 
+      nAttr += 1
